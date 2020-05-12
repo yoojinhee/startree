30e02fb26b4f660dc161be9c41eb69d08493cc80
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-12 12:02:32 +0900

    별 찍기

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,6 +10,21 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            Console.Write("입력 :");
+            int line = int.Parse(Console.ReadLine());
+
+            for(int i=0; i<line; i++)//4
+            {
+                for(int j=0; j< line-i-1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j=1; j<= (i+1)*2-1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
